d7f338109bd3f8f5c8c5b37cfe5f33b130462f13
withstu <withstu@users.noreply.github.com>
2014-10-12 17:19:11 +0200

    Create autorate.cmd

---
+++ autorate.cmd
@@ -0,0 +1,33 @@
+:: ====================
+:: AutoRating
+:: ====================
+:: Version 1.0.0.1 - September 2014
+:: Copyright © Sven Wilkens 2014
+:: https://plus.google.com/u/0/+SvenWilkens
+
+:: =======
+:: Licence
+:: =======
+:: This program is free software: you can redistribute it and/or modify it under the terms
+:: of the GNU General Public License as published by the Free Software Foundation, either
+:: version 3 of the License, or (at your option) any later version.
+
+:: This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+:: without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+:: See the GNU General Public License for more details.
+
+:: Please visit http://www.gnu.org/licenses/gpl-3.0-standalone.html to view the GNU GPLv3 licence.
+
+:: ===========
+:: Description
+:: ===========
+:: Automatically sets the rating for all tracks in your iTunes library according to how you listen to them.
+:: It also backup the Play and Skip Counts in the Comment field of the mp3 and disable iTunes album rating
+
+:: Related scripts: https://code.google.com/p/autorate/
+
+:: =========
+:: ChangeLog 
+:: =========
+:: Version 1.0.0.1 - Initial version*/
+cscript Autorate.vbs
